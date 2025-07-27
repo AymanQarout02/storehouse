ef6a95244463a296e69969a38b11f9bbbfb8d6d7
ayman <aymanwkm2002@gmail.com>
2025-07-27 16:26:55 +0300

    add BaseModel

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,10 +6,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Category extends Model
+class Category extends BaseModel
 {
-    use HasFactory,SoftDeletes;
-
     protected $fillable = [
         'name'
     ];
